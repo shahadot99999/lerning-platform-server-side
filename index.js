c5b665a744245e5d2d40764d2b665e405c220a25
shahadot99999 <shahadot99999@gmail.com>
2022-10-25 14:40:40 +0600

    another data loaded and connection between and display show

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const educations = require('./data/educations.json');
 
 app.get('/', (req, res) => {
     res.send('Education API Running');
@@ -13,6 +14,25 @@ app.get('/', (req, res) => {
 app.get('/educations-categories', (req, res) => {
     res.send(categories);
 })
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '06') {
+        res.send(educations)
+    }
+    else {
+        const category_educations = educations.filter(n => n.category_id === id);
+        res.send(category_educations);
+    }
+
+
+})
+app.get('/educations/:id', (req, res) => {
+    const id = req.params.id;
+    const selectededucations = educations.find(n => n._id === id);
+    res.send(selectededucations);
+})
+
+
 app.listen(port, () => {
     console.log('Education News Server running on port', port);
 })
