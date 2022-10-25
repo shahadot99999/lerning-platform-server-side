b75540bdcd32aa150078e704edeed6e097eccde8
shahadot99999 <shahadot99999@gmail.com>
2022-10-25 09:25:31 +0600

    categories fake data loaded and show local 5000

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('Education API Running');
 });
+app.get('/educations-categories', (req, res) => {
+    res.send(categories);
+})
 app.listen(port, () => {
     console.log('Education News Server running on port', port);
 })
